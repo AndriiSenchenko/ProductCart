f304eae6042ac9ec3f33eb20e64bb605cad795d6
Andrii <andre.senchenko@gmail.com>
2023-10-07 12:25:54 +0300

    Initial Commit

---
+++ ProductCart/ProductCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCartApp.swift
+//  ProductCart
+//
+//  Created by Andrii on 07.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProductCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
